b47c6200fbe741cfac3a438fdaed54adfa9d7626
sim-t <s4sitrum@uni-trier.de>
2018-12-10 13:51:23 +0100

    changed names

---
+++ sonar-maven.sh
@@ -0,0 +1,7 @@
+#mvn clean install sonar:sonar -Dsonar.host.url=https:/sonarqube.com -Dsonar.organization=marshalexception-github -Dsonar.login=SONARCLOUD-TOKEN
+
+mvn sonar:sonar \
+  -Dsonar.projectKey=marshalexception_fst18-public \
+  -Dsonar.organization=marshalexception-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=55b3c8aba9993716217aaa9088f40e530bb67712
